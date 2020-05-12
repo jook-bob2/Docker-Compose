97779251ab73ce3307fbe359e3b84b8d551f1e78
ehrmaks <qwefk123@naver.com>
2020-05-12 22:10:24 +0900

    uploadpath update

--- springboot/src/main/java/com/management/controller/ManagementController.java
+++ springboot/src/main/java/com/management/controller/ManagementController.java
@@ -63,7 +63,7 @@ public class ManagementController {
 		
 		for(MultipartFile file : files) {
 			//String savedName = uploadFile(file.getOriginalFilename(), file.getBytes());
-			String savedName = UploadFileUtils.uploadFile("src/main/resources/static/upload/", file.getOriginalFilename(), file.getBytes());
+			String savedName = UploadFileUtils.uploadFile("build/resources/main/static/upload/", file.getOriginalFilename(), file.getBytes());
 			//String savedName = UploadFileUtils.uploadFile("home/upload/", file.getOriginalFilename(), file.getBytes());
 			System.out.println("===========");
 			dto.setImage("upload/"+savedName);
