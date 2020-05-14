978392bb134c226d45a9d0776d97e242d28dd656
ehrmaks <qwefk123@naver.com>
2020-05-14 22:51:36 +0900

    volume rebuild

--- springboot/src/main/java/com/management/controller/ManagementController.java
+++ springboot/src/main/java/com/management/controller/ManagementController.java
@@ -63,10 +63,10 @@ public class ManagementController {
 		
 		for(MultipartFile file : files) {
 			//String savedName = uploadFile(file.getOriginalFilename(), file.getBytes());
-			String savedName = UploadFileUtils.uploadFile("tmp/", file.getOriginalFilename(), file.getBytes());
+			String savedName = UploadFileUtils.uploadFile("var/lib/datas/", file.getOriginalFilename(), file.getBytes());
 			//String savedName = UploadFileUtils.uploadFile("home/upload/", file.getOriginalFilename(), file.getBytes());
 
-			dto.setImage("tmp/"+savedName);
+			dto.setImage("var/lib/datas"+savedName);
 		}
 		managementService.customerAdd(dto);
 	}
